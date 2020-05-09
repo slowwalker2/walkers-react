1c3f639e4e9498a874c631989647bbfc72deb4b5
slowwalker2 <ruri136@gmail.com>
2020-05-10 02:41:05 +0900

    chart plugin

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import 'codemirror/lib/codemirror.css';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { Editor } from '@toast-ui/react-editor';
+import 'tui-chart/dist/tui-chart.css';
+import chart from '@toast-ui/editor-plugin-chart';
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         height='600px'
         initialEditType='markdown'
         useCommandShortcut={true}
+        plugins={[chart]}
       />
     </div>
   );
