3a80222cfefdb080d51cdb273a5c5cc0014b996c
slowwalker2 <ruri136@gmail.com>
2020-05-10 01:47:27 +0900

    toast editor 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import 'codemirror/lib/codemirror.css';
+import '@toast-ui/editor/dist/toastui-editor.css';
+import { Editor } from '@toast-ui/react-editor';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Editor
+        previewStyle='vertical'
+        height='600px'
+        initialEditType='markdown'
+        useCommandShortcut={true}
+      />
+    </div>
+  );
 }
 
 export default App;
