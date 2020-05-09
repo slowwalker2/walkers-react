247734ba6340e30c6d0a8895433e0b825306717a
slowwalker2 <ruri136@gmail.com>
2020-05-10 02:48:23 +0900

    code-syntax-highlight plugin

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import '@toast-ui/editor/dist/toastui-editor.css';
 import { Editor } from '@toast-ui/react-editor';
 import 'tui-chart/dist/tui-chart.css';
 import chart from '@toast-ui/editor-plugin-chart';
+import 'highlight.js/styles/github.css';
+import codeSyntaxHightlight from '@toast-ui/editor-plugin-code-syntax-highlight';
+import hljs from 'highlight.js';
 
 function App() {
   return (
@@ -13,7 +16,7 @@ function App() {
         height='600px'
         initialEditType='markdown'
         useCommandShortcut={true}
-        plugins={[chart]}
+        plugins={[chart, [codeSyntaxHightlight, { hljs }]]}
       />
     </div>
   );
