8131ef74fa00dd13e285f463e42a9437a5e046fe
slowwalker2 <ruri136@gmail.com>
2020-05-10 22:31:12 +0900

    Editor 컴포넌트화

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import React from 'react';
-import 'codemirror/lib/codemirror.css';
-import '@toast-ui/editor/dist/toastui-editor.css';
-import { Editor } from '@toast-ui/react-editor';
-import 'tui-chart/dist/tui-chart.css';
-import chart from '@toast-ui/editor-plugin-chart';
-import 'highlight.js/styles/github.css';
-import codeSyntaxHightlight from '@toast-ui/editor-plugin-code-syntax-highlight';
-import hljs from 'highlight.js';
-import 'tui-color-picker/dist/tui-color-picker.css';
-import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
-import tableMergedCell from '@toast-ui/editor-plugin-table-merged-cell';
-import uml from '@toast-ui/editor-plugin-uml';
+import Editor from './components/Editor';
 
 function App() {
   return (
     <div className='App'>
-      <Editor
-        previewStyle='vertical'
-        height='600px'
-        initialEditType='markdown'
-        useCommandShortcut={true}
-        plugins={[chart, [codeSyntaxHightlight, { hljs }], colorSyntax, tableMergedCell, uml]}
-      />
+      <Editor />
     </div>
   );
 }
---
+++ src/components/Editor.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import 'codemirror/lib/codemirror.css';
+import '@toast-ui/editor/dist/toastui-editor.css';
+import { Editor } from '@toast-ui/react-editor';
+import 'tui-chart/dist/tui-chart.css';
+import chart from '@toast-ui/editor-plugin-chart';
+import 'highlight.js/styles/github.css';
+import codeSyntaxHightlight from '@toast-ui/editor-plugin-code-syntax-highlight';
+import hljs from 'highlight.js';
+import 'tui-color-picker/dist/tui-color-picker.css';
+import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
+import tableMergedCell from '@toast-ui/editor-plugin-table-merged-cell';
+import uml from '@toast-ui/editor-plugin-uml';
+
+function EditorComponent(props) {
+  return (
+      <Editor
+        previewStyle='vertical'
+        height='600px'
+        initialEditType='markdown'
+        {...props}
+        plugins={[chart, [codeSyntaxHightlight, { hljs }], colorSyntax, tableMergedCell, uml]}
+      ></Editor>
+  );
+}
+
+export default EditorComponent;
