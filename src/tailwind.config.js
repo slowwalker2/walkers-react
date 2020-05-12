6ed0b5b83b06c72eac7096d7ade334582a6f3b27
slowwalker2 <ruri136@gmail.com>
2020-05-12 18:52:11 +0900

    tailwind.config.js 수정

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,8 +1,31 @@
 module.exports = {
-  theme: {},
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    fontFamily: {
+      display: ['Gilroy', 'sans-serif'],
+      body: ['Graphik', 'sans-serif'],
+    },
+    borderWidth: {
+      default: '1px',
+      '0': '0',
+      '1': '1px',
+      '2': '2px',
+    },
+    extend: {
+      colors: {
+        cyan: '#9cdbff',
+      },
+      spacing: {
+        '96': '24rem',
+        '128': '32rem',
+      },
+    },
+  },
   variants: {},
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
-}
+  plugins: [require('tailwindcss'), require('autoprefixer')],
+};
