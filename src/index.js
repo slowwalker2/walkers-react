db1871f219fade649b03b90ad24ccfa926c252f9
slowwalker2 <ruri136@gmail.com>
2020-05-12 18:52:00 +0900

    SignIn 컴포넌트 & 페이지

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/main.css';
 import App from './App';
+import SignInPage from './pages/SignInPage';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SignInPage />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
